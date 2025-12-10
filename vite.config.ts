f1fe48ca09e95b1634bb224be9afe2deca0fe397
Just in MA <mazhaokang76@gmail.com>
2025-12-10 16:14:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,14 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 3000,
     host: '0.0.0.0',
   },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    }
-  },
   build: {
+    outDir: 'dist',
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-          'charts': ['recharts']
-        }
-      }
-    }
   }
 });
