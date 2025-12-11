ceb31cd34527f88eab676e296d241a0249ea70e5
Just in MA <mazhaokang76@gmail.com>
2025-12-11 09:20:40 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,16 +4,22 @@ import MMSEAssessment from './components/MMSEAssessment';
 import MOCAAssessment from './components/MOCAAssessment';
 import ADLAssessment from './components/ADLAssessment';
 
+console.log('App component loaded');
+
 type ViewType = 'home' | 'MMSE' | 'MOCA' | 'ADL';
 
 function App() {
   const [currentView, setCurrentView] = useState<ViewType>('home');
 
+  console.log('Current view:', currentView);
+
   const handleSelectAssessment = (type: ViewType) => {
+    console.log('Switching to view:', type);
     setCurrentView(type);
   };
 
   const handleBackToHome = () => {
+    console.log('Returning to home');
     setCurrentView('home');
   };
 
