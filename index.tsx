832aaa201bfa7622af8eb7a33b97e6d96fe69d4c
Just in MA <mazhaokang76@gmail.com>
2025-12-11 09:22:20 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+console.log('Application starting...');
+
 const rootElement = document.getElementById('root');
 
 if (!rootElement) {
+  console.error('Root element not found');
   throw new Error('Root element not found');
 }
+
+console.log('Root element found, mounting React app...');
 
 const root = createRoot(rootElement);
 root.render(
@@ -14,3 +19,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+console.log('React app mounted');
